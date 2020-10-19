0a9972f7cd75c9dc19374a4740ab2adcf6df495f
emghee <55850407+emghee@users.noreply.github.com>
2020-10-19 10:45:01 -0400

    projectile script stuff

---
+++ Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour{
+
+	public float bulletForce = 20f;
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+
+	void Update(){
+		if ((Input.GetButtonDown("Fire1")) || (Input.GetKeyDown("space"))) {
+			Shoot();
+		}
+	}
+
+	// Create the bullet and send it flying
+	void Shoot(){
+		GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+		rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+	}
+
+}
