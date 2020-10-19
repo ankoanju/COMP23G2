9b74453d0242bb3a6183673e51bbaf72b11e4567
Fre3land <72616640+Fre3land@users.noreply.github.com>
2020-10-19 11:16:27 -0400

    Update BulletDEA.cs

--- Scripts/BulletDEA.cs
+++ Scripts/BulletDEA.cs
@@ -8,13 +8,13 @@ public class Bullet : MonoBehaviour {
 
 	Rigidbody2D rb;
 
-	Cat target;
+	GameObject target;
 	Vector2 moveDirection;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		target = GameObject.FindObjectOfType<Cat>();
+		target = GameObject.FindObjectOfType<GameObject>();
 		moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
 		rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
 		Destroy (gameObject, 3f);
@@ -22,7 +22,7 @@ public class Bullet : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D col)
 	{
-		if (col.gameObject.name.Equals ("Cat")) {
+		if (col.gameObject.name.Equals ("GameObject")) {
 			Debug.Log ("Hit!");
 			Destroy (gameObject);
 		}
